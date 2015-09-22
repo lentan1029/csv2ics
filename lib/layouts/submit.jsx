80cae003e0771a51b06cbb0930b34ec0c7a653f4
lentan <lentan1029@gmail.com>
2015-09-23 01:14:04 +0200

    changed value of React form for submit page to null

--- lib/layouts/submit.jsx
+++ lib/layouts/submit.jsx
@@ -5,13 +5,13 @@ Submit = React.createClass({
         <div className="form-group">
           <label className="control-label" htmlFor="url">URL</label>
           <div className="controls">
-              <input name="url" id="url" type="text" value="" placeholder="Your URL" className="form-control"/>
+              <input name="url" id="url" type="text" value={null} placeholder="Your URL" className="form-control"></input>
           </div>
         </div>
         <div className="form-group">
           <label className="control-label" htmlFor="title">Title</label>
           <div className="controls">
-              <input name="title" id="title" type="text" value="" placeholder="Name your post" className="form-control"/>
+              <input name="title" id="title" type="text" value={null} placeholder="Name your post" className="form-control"/>
           </div>
         </div>
         <input type="submit" value="Submit" className="btn btn-primary"/>
