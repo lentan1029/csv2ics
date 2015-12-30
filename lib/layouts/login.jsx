5dc100e10e4de84c2ca673830bb77559f81b52b0
lentan <lentan1029@gmail.com>
2015-12-30 13:12:12 +0800

    changed text

--- lib/layouts/login.jsx
+++ lib/layouts/login.jsx
@@ -20,7 +20,7 @@ Login = React.createClass({
     if(this.data.loggedIn){
       return <div className="btn post" onClick={this.redirectToConvert}> Click to proceed. </div>
     } else {
-      return <div> Please log in via Facebook. <br /> (This website works best with Safari) </div>
+      return <div> Please log in via Facebook. <br /> (This website <u>works best with Safari</u>) </div>
     }
   }
 
