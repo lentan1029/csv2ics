1fb67ec0a148208d56da2b398fa7f3462dda04e6
lentan <lentan1029@gmail.com>
2015-12-30 15:24:35 +0800

    added more specific instructions

--- lib/layouts/login.jsx
+++ lib/layouts/login.jsx
@@ -20,7 +20,7 @@ Login = React.createClass({
     if(this.data.loggedIn){
       return <div className="btn post" onClick={this.redirectToConvert}> Click to proceed. </div>
     } else {
-      return <div> Please log in via Facebook. <br /> (This website <u>works best with Safari</u>) </div>
+      return <div> Please log in via Facebook. <br /> (This website <u>works best with Safari on a Mac</u>) </div>
     }
   }
 
